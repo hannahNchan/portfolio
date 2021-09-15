95196d89c5bccb3c45e4840fc2c307d1b88f50e0
Hannah <hannah.n.kahnwald@gmail.com>
2021-09-14 19:21:34 -0500

    Feature: Crear seccion HOME (solo maqueta)

--- src/containers/Content/Home/index.js
+++ src/containers/Content/Home/index.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
+import './styles.scss';
+import PHOTO_BG from '../../../assets/space.jpg';
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <div 
+      style={{ backgroundImage: `url(${PHOTO_BG})` }}
+      className="hannah-home"
+    >
+      <div className="container">
+        <div className="box">
+          <div className="title">
+            <span className="block"></span>
+            <h1>Hannah Narvaez<span></span></h1>
+          </div>
+          <div className="role">
+            <div className="block"></div>
+            <p>Full-Stack Web developer</p>
+          </div>
+        </div>
+      </div>
+        <Link to="/blog">
+          <footer>
+            <div class="texto">
+              <span>Watch my Blog</span>
+            </div>
+          </footer>
+       </Link>
+    </div>
   )
 }
 
